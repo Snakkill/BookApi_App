89cb61fc22d9218e50f6dff4a6b1253e7d050b79
Ayman <aymanmoubayed@gmail.com>
2022-02-22 10:01:49 -0600

    Initial commit

---
+++ app/src/main/java/com/example/bookapiapp/MainActivity.java
@@ -0,0 +1,26 @@
+package com.example.bookapiapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+    EditText mBookInput;
+    TextView mTitleText, mAuthorText;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        mBookInput = findViewById(R.id.userInput);
+
+    }
+
+    public void searchBooks(View view) {
+        String queryString =mBookInput.getText().toString();
+        mTitleText=findViewById(R.id.result1);
+        mAuthorText=findViewById(R.id.result2);
+    }
+}
