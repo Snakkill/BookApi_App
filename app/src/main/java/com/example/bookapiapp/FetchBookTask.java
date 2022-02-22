f72ebd8ad27e0224991f5d0f605b73b1a0932f3e
Ayman <aymanmoubayed@gmail.com>
2022-02-22 15:21:47 -0600

    Parsed JSON, outputs wanted data

--- app/src/main/java/com/example/bookapiapp/FetchBookTask.java
+++ app/src/main/java/com/example/bookapiapp/FetchBookTask.java
@@ -3,6 +3,10 @@ package com.example.bookapiapp;
 import android.os.AsyncTask;
 import android.widget.TextView;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
 public class FetchBookTask extends AsyncTask<String,Void,String> {
 
 private TextView mTitleText;
@@ -23,6 +27,20 @@ private TextView mAuthorText;
     @Override
     protected void onPostExecute(String jsonString) {
         super.onPostExecute(jsonString);
+try{
+        JSONObject jsonObject = new JSONObject(jsonString);
+        JSONArray array = jsonObject.getJSONArray("items");
+        JSONObject firstResult = array.getJSONObject(1);
+        JSONObject volInfo = firstResult.getJSONObject("volumeInfo");
+        String title = volInfo.getString("title");
+        JSONArray authorz = volInfo.getJSONArray("authors");
+        String authors = authorz.toString();
+        mTitleText.setText(title);
+        mAuthorText.setText(authors);
+    } catch (
+    JSONException e) {
+        e.printStackTrace();
+    }
     }
 }
 
